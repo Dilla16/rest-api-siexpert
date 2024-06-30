4d932c2fbd66e12d261bda6a33ee153d9bd2a12a
Dilla16 <fadillahanggraini16@gmail.com>
2024-06-30 23:52:09 +0700

    main update

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ require(`dotenv`).config();
 
 app.use(express.json());
 
-const bookRouter = require("./routes/book.router");
+// const bookRouter = require("./routes/book.router");
 
-app.use("/api/v1/books", bookRouter);
+// app.use("/api/v1/books", bookRouter);
 
 app.listen(process.env.PORT, () => console.log("Server is running on port 3000"));
