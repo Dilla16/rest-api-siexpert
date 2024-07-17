e8d8fdec60964e23c9f624a507a8a36d7ecbdc04
Dilla16 <fadillahanggraini16@gmail.com>
2024-07-17 22:50:58 +0700

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 app.use(express.json());
 app.use("/api", userRoutes);
 
