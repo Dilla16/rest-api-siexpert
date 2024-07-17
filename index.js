e0e9a19ed2f0d9b6b94abbbad15543e8f3e72189
Dilla16 <fadillahanggraini16@gmail.com>
2024-07-17 22:32:44 +0700

    update

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const corsMiddleware = require("./cors");
+const cors = require("cors");
 const userRoutes = require("./app/routes/userRoute");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(corsMiddleware); // Use CORS middleware
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use("/api", userRoutes);
 
